aac7283010b0553388d0cad342d92d187d137a53
ArturN <nizamutdin.art@yandex.ru>
2016-07-17 11:35:34 +0300

    rendered simple list via *ngFor

---
+++ ng-book2/FirstApp/app.ts
@@ -0,0 +1,26 @@
+// your code goes here
+import {bootstrap} from "@angular/platform-browser-dynamic";
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'hello-world',
+    template: `
+    <ul>
+        <li *ngFor="let name of names">Hello {{name}}</li>
+    </ul>
+    `
+})
+
+class HelloWorld {
+    names: string[];
+    constructor(){
+        this.names = ['Ari', 'Carlos', 'Felipe', 'Nate'];
+    }
+}
+ bootstrap(HelloWorld);
+/*
+ class HelloWorld {
+ selector: 'hello-world'
+ }
+
+ bootstrap(HelloWorld);*/
