dcc3d61179853be41e92868122e19803f6507b7a
unknown <Artur_Nizamutdinov@EPRUSARW1180.saratov.epam.com>
2016-07-18 19:03:07 +0300

    started working on "Angular 2 Simple Reddit" app

--- ng-book2/FirstApp/app.ts
+++ ng-book2/FirstApp/app.ts
@@ -3,24 +3,100 @@ import {bootstrap} from "@angular/platform-browser-dynamic";
 import {Component} from "@angular/core";
 
 @Component({
-    selector: 'hello-world',
+    selector: 'reddit-article',
+    host: {
+        class: 'row'
+    },
     template: `
-    <ul>
-        <li *ngFor="let name of names">Hello {{name}}</li>
-    </ul>
+    <div class="four wide column center aligned votes">
+        <div class="ui static">
+            <div class="value">
+                {{votes}}
+            </div>
+            <div class="label">
+                Points
+            </div>
+        </div>
+    </div>
+    <div class="twelve wide column">
+        <a class="ui large header" href="{{ link }}">
+            {{ title }}
+        </a>
+        <ul class="ui big horizontal list voters">
+            <li class="item">
+                <a href (click)="voteUp()">
+                    <i class="arrow up icon"></i>
+                    upvote
+                </a>
+            </li>
+            <li class="item">
+                <a href (click)="voteDown()">
+                    <i class="arrow down icon"></i>
+                    downvote
+                </a>
+            </li>
+        </ul>
+    </div>
     `
 })
 
-class HelloWorld {
-    names: string[];
+class ArticleComponent {
+    votes: number;
+    title: string;
+    link: string;
+
     constructor(){
-        this.names = ['Ari', 'Carlos', 'Felipe', 'Nate'];
+        this.title = 'Angular 2';
+        this.link = 'http://angular.io';
+        this.votes = 10;
+    }
+
+    voteUp() {
+        this.votes++;
+        return false;
+    }
+
+    voteDown() {
+        this.votes--;
+        return false;
     }
 }
- bootstrap(HelloWorld);
-/*
- class HelloWorld {
- selector: 'hello-world'
- }
 
- bootstrap(HelloWorld);*/
+@Component({
+    selector: 'reddit',
+    directives: [ArticleComponent],
+    template: `
+    <form class="ui large form segment">
+        <h3 class="ui header">Add a link</h3>
+
+        <div class="field">
+            <label for="title">Title:</label>
+            <input id="title" name="title" #newtitle>
+        </div>
+        <div class="field">
+            <label for="link">Link:</label>
+            <input id="link" name="link" #newlink>
+        </div>
+
+        <button (click)="addArticle(newtitle, newlink)"
+                class="ui positive right floated button">
+            Submit link
+        </button>
+    </form>
+
+    <div class="ui grid posts">
+        <reddit-article></reddit-article>
+    </div>
+    `
+})
+
+class RedditApp {
+    constructor(){
+    }
+
+    addArticle(title: HTMLInputElement, link: HTMLInputElement): void {
+        console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    }
+}
+
+bootstrap(RedditApp);
