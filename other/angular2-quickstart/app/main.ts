b59566df82dad313a636904802a50cf78b15ab16
ArturN <nizamutdin.art@yandex.ru>
2016-07-24 11:54:18 +0300

    Added angular2-webpack-starter and angular2-quickstart

---
+++ other/angular2-quickstart/app/main.ts
@@ -0,0 +1,6 @@
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app.component';
+bootstrap(AppComponent);
+/**
+ * Created by Артур on 24.07.2016.
+ */
